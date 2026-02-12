0b3802371c2c5cbe1bd283fc9f0440552feed49f
paulroth3d <paulroth3d@gmail.com>
2026-02-12 07:43:05 -0600

    UPDATE: attempting to fix imports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import _chain from './chain.ts';
 // import _importFresh from './importFresh.ts';
-import _importFresh2 from './importFresh.ts';
+import { importFresh2 } from './importFresh.ts';
 
 export const chain = _chain;
-export const importFresh = _importFresh2;
+export const importFresh = importFresh2;
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 // deno-coverage-ignore-start
