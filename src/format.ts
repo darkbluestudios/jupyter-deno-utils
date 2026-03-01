1129bf7ff8c6d9ae6715daf01164753f385f6657
Paul Roth <paulroth3d@gmail.com>
2026-02-28 18:38:43 -0600

    UPDATE: elipsify

--- src/format.ts
+++ src/format.ts
@@ -27,3 +27,128 @@
 export function zeroFill(num:number, len = 3, fill = '0'):string {
   return String(num).padStart(len, fill);
 };
+
+/**
+ * Interface for a number with a remainder
+ */
+export interface Remainder {
+  integer:number;
+  remainder:number;
+}
+
+/**
+ * Divide a number and get the integer value and remainder
+ * @param {Number} numerator - number to be divided
+ * @param {Number} denominator - number to divide with
+ * @returns {Object} - ({value, remainder}) 
+ * @example
+ * utils.format.divideR(5, 3)
+ * // ({ value: 1, remainder: 2 })
+ */
+export function divideWithRemainder(numerator:number, denominator:number): Remainder {
+  return ({
+    integer: Math.trunc(numerator / denominator),
+    remainder: numerator % denominator
+  });
+};
+
+/**
+ * @typedef {Object} Duration
+ * @property {Number} days -
+ * @property {Number} hours -
+ * @property {Number} minutes -
+ * @property {Number} seconds -
+ * @property {Number} milliseconds -
+ * @property {Number} epoch - the total duration in milliseconds
+ */
+export interface Duration {
+    days:number;
+    hours:number;
+    minutes:number;
+    seconds:number;
+    milliseconds:number;
+    epoch:number;
+}
+
+/**
+ * Describes the duration in milliseconds for a given time period.
+ */
+export const DURATION = {
+  MILLISECOND: 1,
+  SECOND: 1000,
+  MINUTE: 1000 * 60,
+  HOUR: 1000 * 60 * 60,
+  DAY: 1000 * 60 * 60 * 24
+};
+
+/**
+ * Determines the length of time that a number of milliseconds take in duration
+ * @param {Number} milliseconds - Number of millisecond duration
+ * @returns {Duration}
+ * @example
+ * d1 = new Date(Date.UTC(2022, 2, 8, 22, 55, 12));
+ * // 2022-03-08T22:55:14.775Z
+ * d2 = new Date(Date.UTC(2022, 2, 8, 23, 00, 0));
+ * // 2022-03-08T23:02:18.040Z
+ * 
+ * utils.format.millisecondDuration(d2.getTime() - d1.getTime())
+ * // {
+ * //   days: 0,
+ * //   hours: 0,
+ * //   minutes: 4,
+ * //   seconds: 48,
+ * //   milliseconds: 0,
+ * //   epoch: 288000
+ * // }
+ */
+export function millisecondDuration(milliseconds:number):Duration {
+  const result:Duration = { days:0, hours:0, minutes:0, seconds:0, milliseconds:0, epoch:0 };
+  let division = divideWithRemainder(milliseconds, DURATION.DAY);
+  result.days = division.integer;
+  division = divideWithRemainder(division.remainder, DURATION.HOUR);
+  result.hours = division.integer;
+  division = divideWithRemainder(division.remainder, DURATION.MINUTE);
+  result.minutes = division.integer;
+  division = divideWithRemainder(division.remainder, DURATION.SECOND);
+  result.seconds = division.integer;
+  result.milliseconds = division.remainder;
+  result.epoch = milliseconds;
+
+  return result;
+};
+
+/**
+ * Ellipsis unicode character `…`
+ * @type {String}
+ * @private
+ * @example
+ * utils.format.ellipsis('supercalifragilisticexpialidocious', 5)
+ * // super…
+ */
+export const ELLIPSIS = '…';
+
+/**
+ * Ellipsifies a string (but only if it is longer than maxLen)
+ * 
+ * @param {String} str - string to be ellipsified
+ * @param {Integer} [maxLen = 50] - the maximum length of str before getting ellipsified
+ * @returns {String}
+ * @example
+ * utils.format.ellipsify('longName') // 'longName' (as maxLen is 50)
+ * utils.format.ellipsify('longName', 8) // 'longName' (as maxLen is 8)
+ * utils.format.ellipsify('longName', 4) // 'long…' (as str is longer than maxLen)
+ */
+export function ellipsify(str:any, maxLen:number = 50):string {
+  const cleanStr = !str
+    ? ''
+    : typeof str === 'string'
+      ? str
+      : JSON.stringify(str);
+  
+  const cleanLen = maxLen > 0 ? maxLen : 50;
+
+  if (cleanStr.length > cleanLen) {
+    return `${cleanStr.substring(0, cleanLen)}…`;
+  }
+  return cleanStr;
+};
--- tests/format__spec.ts
+++ tests/format__spec.ts
@@ -20,4 +20,139 @@ describe('format', () => {
       expect(results).toBe(expected);
     });
   });
+
+  describe('divideR', () => {
+    it('divide 5/3', () => {
+      const expected = ({ integer: 1, remainder: 2 });
+      const result = FormatUtils.divideWithRemainder(5, 3);
+      expect(result).toEqual(expected);
+    });
+    it('divide 0/3', () => {
+      const expected = ({ integer: 0, remainder: 0 });
+      const result = FormatUtils.divideWithRemainder(0, 3);
+      expect(result).toEqual(expected);
+    });
+    it('divide 3/0', () => {
+      const expected = ({ integer: Infinity, remainder: NaN });
+      const result = FormatUtils.divideWithRemainder(3, 0);
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('millisecondDuration', () => {
+    it('duration of 2 seconds', () => {
+      const duration = 2000;
+      const results = FormatUtils.millisecondDuration(duration);
+      const expected = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 2,
+        milliseconds: 0,
+        epoch: duration
+      };
+      expect(results).toStrictEqual(expected);
+    });
+    it('duration of 2 minutes', () => {
+      const duration = 1000 * 60 * 2;
+      const results = FormatUtils.millisecondDuration(duration);
+      const expected = {
+        days: 0,
+        hours: 0,
+        minutes: 2,
+        seconds: 0,
+        milliseconds: 0,
+        epoch: duration
+      };
+      expect(results).toStrictEqual(expected);
+    });
+    it('duration of 2 hours', () => {
+      const duration = 1000 * 60 * 60 * 2;
+      const results = FormatUtils.millisecondDuration(duration);
+      const expected = {
+        days: 0,
+        hours: 2,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+        epoch: duration
+      };
+      expect(results).toStrictEqual(expected);
+    });
+    it('duration of 2 days', () => {
+      const duration = 1000 * 60 * 60 * 24 * 2;
+      const results = FormatUtils.millisecondDuration(duration);
+      const expected = {
+        days: 2,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+        epoch: duration
+      };
+      expect(results).toStrictEqual(expected);
+    });
+    it('complicated', () => {
+      const day1 = new Date(2022, 2, 27, 22, 43, 40);
+      const day2 = new Date(2022, 4, 3, 9);
+      const duration = day2.getTime() - day1.getTime();
+      const results = FormatUtils.millisecondDuration(duration);
+      const expected = {
+        days: 36,
+        hours: 10,
+        minutes: 16,
+        seconds: 20,
+        milliseconds: 0,
+        epoch: duration
+      };
+      expect(results).toStrictEqual(expected);
+    });
+  });
+
+  describe('ellipsify', () => {
+    it('ellipsifies a long string', () => {
+      const str = '0123456789';
+      const expected = '01234' + FormatUtils.ELLIPSIS; // eslint-disable-line
+      const results = FormatUtils.ellipsify(str, 5);
+      expect(results).toBe(expected);
+    });
+    it('does not ellipsify a short enough string', () => {
+      const str = '0123456789';
+      const expected = '0123456789';
+      const results = FormatUtils.ellipsify(str, 20);
+      expect(results).toBe(expected);
+    });
+    it('does not ellipsify a string of exact length', () => {
+      const str = '0123456789';
+      const expected = '0123456789';
+      const results = FormatUtils.ellipsify(str, 10);
+      expect(results).toBe(expected);
+    });
+    it('ellipsifies by default on long strings', () => {
+      const str = '012345678901234567890123456789012345678901234567891';
+      const expected = `01234567890123456789012345678901234567890123456789${FormatUtils.ELLIPSIS}`;
+      const results = FormatUtils.ellipsify(str);
+      expect(results).toBe(expected);
+    });
+    it('can ellipsify on null', () => {
+      const str = null;
+      const expected = '';
+      const results = FormatUtils.ellipsify(str, 10);
+      expect(results).toBe(expected);
+    });
+    it('can ellipsify on undefined', () => {
+      const str = undefined;
+      const expected = '';
+      const results = FormatUtils.ellipsify(str, 10);
+      expect(results).toBe(expected);
+    });
+    it('can ellipsify an object', () => {
+      const str = { first: 'name', last: 'name' };
+      const expected = '{"first":"…';
+      const results = FormatUtils.ellipsify(str, 10);
+      expect(results).toBe(expected);
+    });
+  });
 });
+
+
